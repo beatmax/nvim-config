8df82cd7a43bbbb8b40e02242b23eb65d3a0ff87
Maximiliano Pin <maximiliano.pin@asml.com>
2025-07-14 13:43:06 +0200

    Clangd config example

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,13 @@
 require("nvchad.configs.lspconfig").defaults()
 
+-- vim.lsp.config.clangd = {
+--   cmd = {
+--     "clangd",
+--     "--clang-tidy",
+--   },
+-- }
+
 local servers = { "html", "cssls", "clangd" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
